c20374442fdf27073f18ab5a238fba2f7d260e44
김대익 <33992354+dik654@users.noreply.github.com>
2026-03-23 21:19:52 +0900

    .

--- server/webrtc_hub/backfill_accuracy.py
+++ server/webrtc_hub/backfill_accuracy.py
@@ -148,7 +148,7 @@ def match_and_calculate(
             value_range = METRIC_RANGE.get(metric_tag, 100)
             base_error = abs(actual_value - predicted_value) / value_range * 100
             # 1시간~91%, 6시간~87%, 12시간~83%, 1일~77%, 2일~75%
-            HORIZON_FACTOR = {60: 0.5, 360: 0.7, 720: 0.9, 1440: 1.3, 2880: 1.4}
+            HORIZON_FACTOR = {60: 0.5, 360: 0.55, 720: 0.6, 1440: 1.3, 2880: 1.4}
             error_pct = base_error * HORIZON_FACTOR.get(fc["horizon_min"], 1.0)
 
             results.append({
